b8ff70c75a8181db937e188c0d372c4e3fa4266f
bh <bh@metcalfe.ilab.cs.brown.edu>
2009-06-19 12:24:23 -0400

    Restructured Dropboxes so that entries contain pointers to boxes.

--- src/org/wescheme/dropbox/Entry.java
+++ src/org/wescheme/dropbox/Entry.java
@@ -18,13 +18,26 @@ public class Entry {
 	Calendar date_;
 	@Persistent
 	Program project_;
+	
+	// kludgey back pointers.
+	@Persistent
+	long dbID_;
+	@Persistent
+	Integer binID_;
+	
 	// @Persistent
 	//String comments_; //TODO Do we want a string that is guaranteed by its type to be web safe?
 	
 	
-	public Entry(Program p){
-		date_ = Calendar.getInstance();
-		project_ = p;
+	public Entry(Dropbox db, Integer binID, Program p){
+		date_ = Calendar.getInstance(); // The entry was created /now/!
+		project_ = p.clone(db.owner()); // clone the program into the dropbox.
+		//TODO The notion of ownership becomes murky here.
+		//TODO Can I delete an item in someone's dropbox if I'm the author and not the owner?
+		
+		dbID_ = db.id();
+		binID_ = binID;
+		
 		//comments_ = comments;
 	}
 	
