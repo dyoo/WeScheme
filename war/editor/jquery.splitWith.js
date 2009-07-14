d812dd4769a88bc0e2aa43255ffde1e902f3677b
Brendan Hickey <brendan@bhickey.net>
2009-07-14 16:29:28 -0400

    Updated splitWith to support object splitters

--- war/editor/jquery.splitWith.js
+++ war/editor/jquery.splitWith.js
@@ -4,6 +4,7 @@
 	
 	    var prefix;
 	    var suffix;
+	    var toFocus = elem;
 	
 		if (typeof split == "string"){
 			
@@ -18,17 +19,39 @@
 			 var nodes = $(this).contents();
 			 prefix = nodes.get(0).nodeValue;
 			 suffix = nodes.get(2).nodeValue;
+			 
 		}
 		
-	this.empty();
+	
     var prefNode = this.clone(true).text(prefix);
+    
+    
     var sufNode  = this.clone(true).text(suffix);
+    this.empty();
+    
+    if( toFocus == null ){
+      toFocus = sufNode;
+    }
+    
     this.append(prefNode); 
     this.append(elem);
     this.append(sufNode);
     this.unbind("keypress");
     this.addClass("wrapper");
     this.attr("contenteditable","false");
+    
+    toFocus.focus();
+
+    if (document.selection) {
+      var range = document.body.createTextRange(toFocus);
+      range.moveToElementText();
+      range.select();
+    } else if (window.getSelection) {
+      var range = document.createRange();
+      range.selectNodeContents($(toFocus).get(0));
+      window.getSelection().addRange(range);
+    }
+   
  
   }
 
