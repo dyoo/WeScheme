f542474cf7e209451e01fb6a0f7fc2ab8f43936c
Danny Yoo <dyoo@cs.wpi.edu>
2009-08-03 19:42:28 -0400

    Changed the relationship to one-to-many because JDO likes it better that way.  Ugh.

--- src/org/wescheme/servlet/SaveProjectServlet.java
+++ src/org/wescheme/servlet/SaveProjectServlet.java
@@ -3,6 +3,7 @@ package org.wescheme.servlet;
 import java.io.IOException;
 
 import javax.jdo.PersistenceManager;
+import javax.jdo.Transaction;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -56,21 +57,22 @@ public class SaveProjectServlet extends HttpServlet{
 			pm.close();
 		}		
 	}
-	
+
 	private Program saveNewProgram(PersistenceManager pm, Session userSession, String title, String code) {
-			Program prog = new Program(code, userSession);
-			prog.updateTitle(title);
-			pm.makePersistent(prog);
-			return prog;
+		Program prog = new Program(code, userSession);
+		prog.updateTitle(title);
+		pm.makePersistent(prog);
+		return prog;
 	}
-	
+
 	private Program saveExistingProgram(PersistenceManager pm, String pid, String title, String code) {
-			Long id = (Long) Long.parseLong(pid);
-			Key k = KeyFactory.createKey("Program", id);
-			Program prog = pm.getObjectById(Program.class, k);
-			prog.updateTitle(title);
-			prog.updateSource(code);
-			pm.makePersistent(prog);
-			return prog;
+
+		Long id = (Long) Long.parseLong(pid);
+		Key k = KeyFactory.createKey("Program", id);
+		Program prog = pm.getObjectById(Program.class, k);
+		prog.updateTitle(title);
+		prog.updateSource(code);
+		return prog;
+
 	}
 }
