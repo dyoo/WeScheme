1ba962e9397a597e2c5597ad7f314274d35fcabb
Danny Yoo <dyoo@cs.wpi.edu>
2009-09-03 15:45:02 -0400

    Still trying to make things work with bespin

--- war/bespin/test.js
+++ war/bespin/test.js
@@ -1,35 +1,52 @@
+var _editorComponent;
+var mode = "text";
 
-function toBespin() {
-    dojo.require("bespin.editor.component");
-    var _editorComponent;
-    // Loads and configures the objects that the editor needs
-    // You can use dojo.addOnLoad(...); but for that you will need to load up dojo.js first
-    function doInit() {
-	_editorComponent = new bespin.editor.Component('editor', {
-            language: "scheme",
-            loadfromdiv: true,
-            set: {
-		strictlines: 'on',
-		closepairs: 'on'
-            }
+
+function doInit(aDiv) {
+    _editorComponent = new bespin.editor.Component(aDiv.id, {
+        language: "scheme",
+        loadfromdiv: true,
+        set: {
+	    strictlines: 'on',
+	    closepairs: 'on'
+        }
     });
-    };
-    
-    function copyToTextarea() {
-	dojo.byId('inandout').value = _editorComponent.getContent();
-    }
-    
-    function copyToEditor() {
-	_editorComponent.setContent(dojo.byId('inandout').value);
-    }
+    mode = 'bespin';
+};
 
-    function setSyntax(value) {
-	bespin.publish("settings:language", { language: value });
+
+function toBespin(aDiv) {  
+//     function copyToTextarea() {
+// 	dojo.byId('inandout').value = _editorComponent.getContent();
+//     }
+    
+//     function copyToEditor() {
+// 	_editorComponent.setContent(dojo.byId('inandout').value);
+//     }
+
+//     function setSyntax(value) {
+// 	bespin.publish("settings:language", { language: value });
+//     }
+
+    if (mode != 'bespin') {
+	dojo.require("bespin.editor.component");
+	dojo.addOnLoad(function() { doInit(aDiv); });
     }
-    dojo.addOnLoad(doInit);
 }
 
 
-function toText() {
-    // FILL ME IN
+function toText(aDiv) {
+    if (mode != 'text') {
+	removeAllChildren(aDiv);
+	_editorComponent.dispose();
+	mode = 'text';
+    }
+}
+
+
+function removeAllChildren(node) {
+    var children = node.childNodes;
+    for (var i = 0; i < children.length; i++) {
+	node.removeChild(children[i]);
+    }
 }
