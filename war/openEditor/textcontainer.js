bd134b8793fc6edd0469f5887e97d4a90d7b64a9
Danny Yoo <dyoo@cs.wpi.edu>
2009-09-03 16:19:48 -0400

    Ripping out more implementation of textareacontainer.

--- war/openEditor/textcontainer.js
+++ war/openEditor/textcontainer.js
@@ -15,21 +15,23 @@ var WeSchemeTextContainer;
 	var that = this;
 	this.div = aDiv;
 	this.mode = 'textarea';
-	this.changeListeners = [];
 
 
 	this.container = new FlapjaxValueHandler(
 	    jQuery(aDiv).find("#defn").get(0));
-	this.container.behavior.changes().mapE(function(v) {
-	    that._notifyListeners();
-	});
     };
 
-    WeSchemeTextContainer.prototype._notifyListeners = function() {
-	for(var i = 0; i < this.changeListeners.length; i++) {
-	    this.changeListeners[i].apply(this, []);
+
+    WeSchemeTextContainer.prototype.changeMode = function(mode) {
+	if (mode == this.mode) {
+	    return;
+	} else if (mode == 'bespin') {
+	} else if (mode == 'textarea') {
+	} else if (mode == 'domeditor') {
+	    // When Brendan's editor is ready, use this...
 	}
-    };
+    }
+
 
 
     // Returns a behavior of the source code
@@ -49,28 +51,22 @@ var WeSchemeTextContainer;
 	this.container.attr("value", code);
     };
 
-    // addChangeListener: (-> void) -> void
-    WeSchemeTextContainer.prototype.addChangeListener = function(l) {
-	this.changeListeners.push(l);
-    };
- 
-    // addClass: string -> void
-    WeSchemeTextContainer.prototype.addClass = function(aClass) {
-	this.container.addClass(aClass);
-    };
 
-    // removeClass: string -> void
-    WeSchemeTextContainer.prototype.removeClass = function(aClass) {
-	this.container.removeClass(aClass);
-    };
+    //////////////////////////////////////////////////////////////////////
 
-    // setReadOnly: boolean -> void
-    WeSchemeTextContainer.prototype.setReadOnly = function(b) {
-	if (b) {
-	    this.container.attr("readonly", "true");
-	} else {
-	    this.container.removeAttr("readonly");
-	}
-    };
+    function TextareaImplementation(rawContainer) {
+	this.container = new FlapjaxValueHandler(
+	    rawContainer);
+	this.container.behavior.changes().mapE(function(v) {
+	    that._notifyListeners();
+	});
+    }
+
+
+
+
+
+
+
 
 })();
