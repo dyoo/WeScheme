6483393c8820c2fe204fc02b1bc002b9e7ebdaa6
Danny Yoo <dyoo@cs.wpi.edu>
2009-12-02 12:50:40 -0500

    Adding administration servlet.

---
+++ src/org/wescheme/servlet/Administrate.java
@@ -0,0 +1,114 @@
+package org.wescheme.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import java.util.logging.Logger;
+
+import javax.jdo.Extent;
+import javax.jdo.PersistenceManager;
+import javax.jdo.Query;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.jdom.Element;
+import org.jdom.output.XMLOutputter;
+import org.wescheme.project.Program;
+import org.wescheme.project.ProgramDigest;
+import org.wescheme.user.Session;
+import org.wescheme.user.SessionManager;
+import org.wescheme.util.PMF;
+
+// Hook to do administration.  All operations must be POSTs done by someone logged in as
+// adminstrator.  Meant to be used through AJAX calls.
+public class Administrate extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6106267266981806559L;
+
+	private static final Logger log = Logger.getLogger(Administrate.class.getName());
+	
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		SessionManager sm = new SessionManager();
+		// FIXME: add intentionality
+		
+		Session userSession = sm.authenticate(req, res);
+		if (!userSession.isAdmin()) {
+			log.warning("Nonadministrator " + userSession.getName() + " trying to access the Administer servlet");
+			res.sendError(401);
+			throw new RuntimeException();
+		}
+		
+		if (req.getParameter("action").equals("refreshProgram")) {
+			refreshProgram(req, res);
+		} else if (req.getParameter("action").equals("listPrograms")) {
+			listPrograms(req, res);
+		} 
+	}
+	
+	
+	// refreshProgram: force the building of the binary of the given program if it exists,
+	// and repair broken invariants of the data model.
+	private void refreshProgram(HttpServletRequest req, HttpServletResponse res) {
+		String programId = req.getParameter("pid");
+		if (programId == null) {
+			throw new RuntimeException("pid missing");
+		}
+		PersistenceManager pm = PMF.get().getPersistenceManager();
+		try {
+			Program prog = pm.getObjectById(Program.class,
+					Long.parseLong(req.getParameter("pid")));
+
+			// First, build the program if it's already been built.
+			if (prog.hasBeenBuilt()) {
+				prog.build();
+			}
+			
+			// Also, refresh its clonedAs attribute
+			prog.getClonedAs().clear();
+			Query query = pm.newQuery(Program.class);
+			query.setFilter("backlink_ == programId");
+			query.declareParameters("Long programId");
+			try {
+				@SuppressWarnings({ "unchecked" })
+				List<Program> pl = (List<Program>) query.execute(prog.getId());
+				for( Program p : pl ){
+					prog.getClonedAs().add(p);
+				}
+				
+			} finally {
+				query.closeAll();
+			}			
+		} finally {
+			pm.close();
+		}
+		
+		// TODO: report that the compilation went ok.
+	}
+	
+	
+	// Lists all programs across all of WeScheme.
+	private void listPrograms(HttpServletRequest req, HttpServletResponse res) {
+		PersistenceManager pm = PMF.get().getPersistenceManager();
+		try {
+		    Extent extent = null;
+		    try {
+		    	extent = pm.getExtent(Program.class, false);    
+		    	// FIXME: can this be done with generics?
+		    	for (Object po: extent) {
+		    		Program p = (Program) po;
+		    	}
+		    } finally {
+		    	if (extent != null) { 
+		    		extent.closeAll();
+		    	}
+		    }
+		} finally {
+			pm.close();
+		}
+	}
+	
+}
