44bacf8b2346c188564f2412107ce3782f8cd6d9
dyoo <dyoo@kfisler-ra1.(none)>
2010-01-05 13:27:40 -0500

    Ripping out the standalone compiler javascript file; replacing it with the jar.

--- src/org/wescheme/project/Compiler.java
+++ src/org/wescheme/project/Compiler.java
@@ -1,83 +1,28 @@
 package org.wescheme.project;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.util.HashSet;
-import java.util.Set;
-
+import org.plt.moby.StandaloneCompiler;
+import org.plt.moby.CompilationResult;
+import java.io.IOException;
 import javax.servlet.http.HttpServlet;
 
-import org.mozilla.javascript.*;
-
-@SuppressWarnings("deprecation")
 public class Compiler extends HttpServlet
 {
 	private static final long serialVersionUID = 6867416066840862239L;
-	static Context cx;
-	static Scriptable scope;	// retrieve the scope
-	static Function compiler;
+	static StandaloneCompiler compiler;
 
 	static {
-		try{
-		cx = Context.enter();
-
-		System.out.println("Initializing the compiler.");
-		System.out.println(System.currentTimeMillis());
-		// WORKAROUND: force use of interpretive mode, because the compressed-standalone-compiler is
-		// complex enough that it doesn't compile to class files anymore.
-		cx.setOptimizationLevel(-1);
-
-		scope = cx.initStandardObjects();
-		FileReader fileReader = new FileReader("runtime/compressed-standalone-compiler.js");
-		BufferedReader inputReader = new BufferedReader(fileReader);
-		StringBuffer compilerSrc = new StringBuffer();
-		String line;
-		
-		System.out.println("Reading compiler source");
-		while((line = inputReader.readLine()) != null){
-			compilerSrc.append(line + "\n");
-		}
-		System.out.println("Installing the compiler.");
-		cx.evaluateString(scope, compilerSrc.toString(), "<cmd>", 1, null); // add the compiler to the scope
-		cx.seal(null);
-		
-		Object fObj = scope.get("compile", scope);
-		               
-		
-		compiler = (Function)fObj;
-		System.out.println("Compiler initialized: ");
-		System.out.println(cx.toString());
-		Object functionArgs[] = { "1" };
-		String result = Context.toString(compiler.call(cx, scope, scope, functionArgs));
-		System.out.println(result);
-		System.out.println(System.currentTimeMillis());
-		Context.exit();
-		} catch (Exception e){
-			e.printStackTrace();
-			System.out.println("Compiler initialization failed.");
+		try {
+			System.out.println("Initializing the compiler.");
+			compiler = new StandaloneCompiler();
+			System.out.println("Compiler initialized.");
+		} catch (IOException e) {
+			throw new RuntimeException(e);
 		}
 	}
-	
-	// init() forces compiler initialization
-	public void init() {}
-	
-	public static ObjectCode compile(SourceCode src){
-	    Context cx = Context.enter();
-		try {
-		    String srcText = src.toString();
-		    Object functionArgs[] = { srcText };
-		    Object[] results = cx.getElements(((ScriptableObject)compiler.call(cx, scope, scope, functionArgs)));
-	
-		    String compiledText = results[0].toString();
-		    Object[] jspermissions = cx.getElements((ScriptableObject)results[1]);
 
-		    Set<String> permissions = new HashSet<String>();
-		    for (int i = 0; i < jspermissions.length; i++) {
-		    	permissions.add(jspermissions[i].toString());
-		    }
-		    return new ObjectCode(compiledText, permissions, false);
-		} finally {
-		    Context.exit();
-		}
+
+	public static ObjectCode compile(SourceCode src){
+		CompilationResult result = compiler.compile(src.toString());
+		return new ObjectCode(result.getJs(), result.getPerms(), false);
 	}
 }
