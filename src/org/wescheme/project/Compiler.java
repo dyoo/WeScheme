d27a8badd5a8c8455b89d2c5b17ec5dc9593e02c
Danny Yoo <dyoo@cs.wpi.edu>
2010-01-06 15:53:16 -0500

    Lazy construction of the compiler, because otherwise everything takes too darn long.

--- src/org/wescheme/project/Compiler.java
+++ src/org/wescheme/project/Compiler.java
@@ -8,20 +8,25 @@ import javax.servlet.http.HttpServlet;
 public class Compiler extends HttpServlet
 {
 	private static final long serialVersionUID = 6867416066840862239L;
-	static StandaloneCompiler compiler;
+	// Singleton compiler object
+	static StandaloneCompiler compiler = null;
 
-	static {
-		try {
-			System.out.println("Initializing the compiler.");
-			compiler = new StandaloneCompiler();
-			System.out.println("Compiler initialized.");
-		} catch (IOException e) {
-			throw new RuntimeException(e);
+
+	private static void initCompiler() {
+		if (compiler == null) {
+			try {
+				System.out.println("Initializing the compiler.");
+				compiler = new StandaloneCompiler();
+				System.out.println("Compiler initialized.");			
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
 		}
 	}
-
-
+	
+	
 	public static ObjectCode compile(SourceCode src){
+		initCompiler();
 		CompilationResult result = compiler.compile(src.toString());
 		return new ObjectCode(result.getJs(), result.getPerms(), false);
 	}
