a7bef448a2f0b22e671194e296733a88aac0cea4
Danny Yoo <dyoo@cs.wpi.edu>
2010-01-29 22:44:20 -0500

    Updating moby

--- war/runtime/collects/bootstrap/function-teachpack.js
+++ war/runtime/collects/bootstrap/function-teachpack.js
@@ -56,21 +56,20 @@ var world_question_ = function(obj) {
 
 var draw_dash_world = function(w) { return text_dash_add(plt.Kernel.apply(world_dash_rocket_dash_height(w),                    plt.Kernel.list([world_dash_current_dash_height(w)]),                    []),rocket_dash_add(w,IMAGE0)); };
 var text_dash_add = function(height, IMAGE0) { return plt.world.Kernel.placeImage(plt.world.Kernel.text(plt.Kernel.string_dash_append([_SHARED[6],plt.Kernel.number_dash__greaterthan_string(height)]),_SHARED[7],(plt.types.Symbol.makeInstance("black"))),_SHARED[8],_SHARED[9],IMAGE0); };
-var rocket_dash_add = function(w, IMAGE0) { return (plt.Kernel._greaterthan__equal_(plt.world.Kernel.imageHeight(ROCKET),plt.Kernel._dash_(HEIGHT, [plt.Kernel.apply(world_dash_rocket_dash_height(w),                    plt.Kernel.list([world_dash_current_dash_height(w)]),                    [])]), []) ?
+var rocket_dash_add = function(w, IMAGE0) { return (plt.kernel.misc.verifyBooleanBranchValue(plt.Kernel._greaterthan__equal_(plt.world.Kernel.imageHeight(ROCKET),plt.Kernel._dash_(HEIGHT, [plt.Kernel.apply(world_dash_rocket_dash_height(w),                    plt.Kernel.list([world_dash_current_dash_height(w)]),                    [])]), []),(plt.Kernel.list([(plt.types.Symbol.makeInstance("make-Loc")),(plt.types.Rational.makeInstance(1448, 1)),(plt.types.Rational.makeInstance(46, 1)),(plt.types.Rational.makeInstance(5, 1)),(plt.types.Rational.makeInstance(88, 1)),(plt.types.String.makeInstance("collects/bootstrap/function-teachpack.ss"))]))) ?
  plt.world.Kernel.placeImage(ROCKET,_SHARED[10],_SHARED[1],IMAGE0) :
- (plt.types.Logic.TRUE ?
+ (plt.kernel.misc.verifyBooleanBranchValue(plt.types.Logic.TRUE,(plt.Kernel.list([(plt.types.Symbol.makeInstance("make-Loc")),(plt.types.Rational.makeInstance(1584, 1)),(plt.types.Rational.makeInstance(48, 1)),(plt.types.Rational.makeInstance(5, 1)),(plt.types.Rational.makeInstance(4, 1)),(plt.types.String.makeInstance("collects/bootstrap/function-teachpack.ss"))]))) ?
  plt.world.Kernel.placeImage(ROCKET,_SHARED[10],plt.Kernel._dash_(HEIGHT, [plt.Kernel.apply(world_dash_rocket_dash_height(w),                    plt.Kernel.list([world_dash_current_dash_height(w)]),                    [])]),IMAGE0) :
- plt.Kernel.error((plt.types.Symbol.makeInstance("cond")),_SHARED[11]))); };
-var tock = function(w, ke) { return make_dash_world(plt.Kernel._plus_([_SHARED[12],world_dash_current_dash_height(w)]),world_dash_rocket_dash_height(w)); };
+ plt.kernel.misc.throwCondExhaustedError((plt.Kernel.list([(plt.types.Symbol.makeInstance("make-Loc")),(plt.types.Rational.makeInstance(1437, 1)),(plt.types.Rational.makeInstance(45, 1)),(plt.types.Rational.makeInstance(2, 1)),(plt.types.Rational.makeInstance(247, 1)),(plt.types.String.makeInstance("collects/bootstrap/function-teachpack.ss"))]))))); };
+var tock = function(w, ke) { return make_dash_world(plt.Kernel._plus_([_SHARED[11],world_dash_current_dash_height(w)]),world_dash_rocket_dash_height(w)); };
 var start = function(rocket_dash_height) { return plt.world.MobyJsworld.bigBang(make_dash_world(_SHARED[9],rocket_dash_height), [plt.world.config.Kernel.onKey((plt.types.liftToplevelToFunctionValue(tock,(plt.types.String.makeInstance("tock")),2,(plt.types.Rational.makeInstance(2, 1))))),plt.world.config.Kernel.onRedraw((plt.types.liftToplevelToFunctionValue(draw_dash_world,(plt.types.String.makeInstance("draw-world")),1,(plt.types.Rational.makeInstance(1, 1)))))]); };_SHARED[10] = (plt.types.Rational.makeInstance(100, 1));
 _SHARED[2] = (plt.types.Rational.makeInstance(600, 1));
-_SHARED[11] = (plt.types.String.makeInstance("cond: fell out of cond"));
 _SHARED[3] = (plt.types.String.makeInstance("http://www.wescheme.org/images/teachpacks/rocket.png"));
 _SHARED[6] = (plt.types.String.makeInstance("Height: "));
 _SHARED[1] = (plt.types.Rational.makeInstance(200, 1));
 _SHARED[7] = (plt.types.Rational.makeInstance(14, 1));
 _SHARED[8] = (plt.types.Rational.makeInstance(60, 1));
-_SHARED[12] = (plt.types.Rational.makeInstance(1, 1));
+_SHARED[11] = (plt.types.Rational.makeInstance(1, 1));
 _SHARED[4] = (plt.types.Rational.makeInstance(2, 1));
 _SHARED[9] = (plt.types.Rational.makeInstance(0, 1));
 
