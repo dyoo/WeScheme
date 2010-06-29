10ebb64bea341b0a1d0f7eff88a55b859a6793ea
Danny Yoo <dyoo@cs.wpi.edu>
2010-06-29 16:27:09 -0400

    Adding delete

---
+++ src/org/wescheme/servlet/Delete.java
@@ -0,0 +1,55 @@
+package org.wescheme.servlet;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
+import javax.jdo.PersistenceManager;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.jdom.output.XMLOutputter;
+import org.wescheme.project.Program;
+import org.wescheme.user.Session;
+import org.wescheme.user.SessionManager;
+import org.wescheme.util.PMF;
+
+/**
+ * Deletes a program by setting its isDeleted flag.
+ * 
+ * @author dyoo
+ *
+ */
+
+public class Delete extends HttpServlet{
+	private static final Logger log = Logger.getLogger(Share.class.getName());
+	private static final long serialVersionUID = -5765142296681571504L;
+
+	public void doPost(HttpServletRequest req, HttpServletResponse resp)	throws IOException 
+	{
+		PersistenceManager pm = PMF.get().getPersistenceManager();
+		Session userSession;
+		SessionManager sm = new SessionManager();
+		try {
+			
+			userSession = sm.authenticate(req, resp);
+			if( null != userSession ) {
+				Program prog = pm.getObjectById(Program.class,
+						Long.parseLong(req.getParameter("pid")));		
+				if (prog.getOwner().equals(userSession.getName())) {
+					prog.setIsDeleted(true);
+					XMLOutputter outputter = new XMLOutputter();
+					resp.getWriter().print(outputter.outputString(prog.toXML(pm)));
+				} else {
+					log.warning(userSession.getName() + " does not own project " + req.getParameter("pid"));
+					throw new RuntimeException("Doesn't own Project");
+				}
+			} else {
+				log.warning("Unauthorized users may not delete a project.");
+				resp.sendError(403);
+			}
+		} finally {
+			pm.close();
+		}
+	}	
+}
