1ef2b5145e5ecccd17e54d8440719eae296581f3
Danny Yoo <dyoo@cs.wpi.edu>
2010-09-21 15:07:40 -0400

    added compatibility fix for IE involving Array.indexOf

--- war/js/compat/ie-fixes.js
+++ war/js/compat/ie-fixes.js
@@ -15,3 +15,16 @@ if (window && !window['Node']) {
     Node.NOTATION_NODE = 12;
 }
     
+
+// Adding Array.indexOf if it doesn't exist yet.
+// http://soledadpenades.com/2007/05/17/arrayindexof-in-internet-explorer/
+if(!Array.indexOf){
+    Array.prototype.indexOf = function(obj){
+	for(var i=0; i < this.length; i++){
+	    if(this[i] == obj){
+	        return i;
+	    }
+	}
+	return -1;
+    }
+}
