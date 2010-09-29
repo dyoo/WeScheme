8de568dc6ce8fc07b05b593eb4cc7eb9cfa70643
Danny Yoo <dyoo@cs.wpi.edu>
2010-09-29 16:28:43 -0400

    adjusted the onbeforeunload so it asks before the user leaves the editor.

--- war-src/js/openEditor/index.js
+++ war-src/js/openEditor/index.js
@@ -19,211 +19,223 @@ goog.require('plt.wescheme.browserCheck');
 
 // FIXME: these should NOT be global variables, but at the moment, they're exposed
 // as such, and the topKeymap refers to myEditor.
+
 var myEditor;
-var defnSourceContainer;
+
+var initializeEditor;
 
 
 
-var initializeEditor = function(attrs) {
-    plt.wescheme.browserCheck();
-    editorSetup(attrs, splitPaneSetup);
-};
+(function() {
+    initializeEditor = function(attrs) {
+	plt.wescheme.browserCheck();
+	unloadSetup();
+	editorSetup(attrs, splitPaneSetup);
+    };
+
+
+
+    var unloadSetup = function() {
+	var doubleCheck = function(){
+	    return "Are you sure you want to leave the Editor? (all unsaved changes will be lost)";
+	}
+	document.body.onbeforeunload = doubleCheck;
+    };
 
 
 
 
-var editorSetup = function(attrs, after) {
-    var userName = attrs['userName'];
-    var pid = attrs['pid'];
-    var publicId = attrs['publicId'];
+    var editorSetup = function(attrs, after) {
+	var userName = attrs['userName'];
+	var pid = attrs['pid'];
+	var publicId = attrs['publicId'];
 
 
-    // Fixme: trigger file load if the pid has been provided.
+	// Fixme: trigger file load if the pid has been provided.
 
-    var statusBar = new plt.wescheme.WeSchemeStatusBar(jQuery("#statusbar"));
+	var statusBar = new plt.wescheme.WeSchemeStatusBar(jQuery("#statusbar"));
 
-    var textContainerOptions = { width: "100%", 
-				 lineNumbers: true };
-    new plt.wescheme.WeSchemeTextContainer(
-	jQuery("#definitions").get(0),
-	textContainerOptions,
-	function(container) {
-	    defnSourceContainer = container;
-	    myEditor = new plt.wescheme.WeSchemeEditor(
-		{ userName: userName,
-		  defn: defnSourceContainer,
-		  interactions: jQuery("#inter").get(0),
-		  filenameInput: jQuery("#filename")},
-		function(_myEditor) {
-		    myEditor = _myEditor;
-
-
-		    jQuery("#run").click(function()  { myEditor.run(); });
-		    jQuery("#stop").click(function()  { myEditor.requestBreak(); });
-		    jQuery("#save").click(function() { myEditor.save(); });
-		    jQuery("#share").click(function()  { myEditor.share(); });
-		    jQuery("#logout").click(function() { submitPost("/logout"); });
-		    jQuery("#bespinMode").click(function() { defnSourceContainer.setMode("bespin")});
-
-		    if (pid !== null) {
-			myEditor.load({pid : parseInt(pid) });
-		    } else if (publicId != null) {
-			myEditor.load({publicId : publicId});
-		    } else {
-			// otherwise, dont load.
-		    }
+	var textContainerOptions = { width: "100%", 
+				     lineNumbers: true };
+	new plt.wescheme.WeSchemeTextContainer(
+	    jQuery("#definitions").get(0),
+	    textContainerOptions,
+	    function(container) {
+		var defnSourceContainer = container;
+		myEditor = new plt.wescheme.WeSchemeEditor(
+		    { userName: userName,
+		      defn: defnSourceContainer,
+		      interactions: jQuery("#inter").get(0),
+		      filenameInput: jQuery("#filename")},
+		    function(_myEditor) {
+			myEditor = _myEditor;
 
 
-		    // Set up the the keymap for the definitions editor so it
-		    // pays attentions to f5.
-		    defnSourceContainer.addKeymap(
-			function(event) {
-			    // handle F5 especially
-			    return (event.type == 'keydown' && event.keyCode === 116);
-			},
-			function(event) {
-			    return plt.wescheme.topKeymap(event);
+			jQuery("#run").click(function()  { myEditor.run(); });
+			jQuery("#stop").click(function()  { myEditor.requestBreak(); });
+			jQuery("#save").click(function() { myEditor.save(); });
+			jQuery("#share").click(function()  { myEditor.share(); });
+			jQuery("#logout").click(function() { submitPost("/logout"); });
+			jQuery("#bespinMode").click(function() { defnSourceContainer.setMode("bespin")});
+
+			if (pid !== null) {
+			    myEditor.load({pid : parseInt(pid) });
+			} else if (publicId != null) {
+			    myEditor.load({publicId : publicId});
+			} else {
+			    // otherwise, dont load.
+			}
+
+
+			// Set up the the keymap for the definitions editor so it
+			// pays attentions to f5.
+			defnSourceContainer.addKeymap(
+			    function(event) {
+				// handle F5 especially
+				return (event.type == 'keydown' && event.keyCode === 116);
+			    },
+			    function(event) {
+				return plt.wescheme.topKeymap(event);
+			    });
+
+
+			// Set up interactions afterwards.
+			jQuery("#interactions").click(function(e) {
+			    myEditor.interactions.focusOnPrompt();
+			    e.stopPropagation();
+			    e.preventDefault();
+			    return false;
 			});
+			jQuery(document.body).keydown(plt.wescheme.topKeymap);
+			
 
-
-		    // Set up interactions afterwards.
-		    jQuery("#interactions").click(function(e) {
-			myEditor.interactions.focusOnPrompt();
-			e.stopPropagation();
-			e.preventDefault();
-			return false;
-		    });
-		    jQuery(document.body).keydown(plt.wescheme.topKeymap);
-		    
-
-		    // Call the after continuation at the end.
-		    after();
-		}
-	    );
-	});
-};
+			// Call the after continuation at the end.
+			after();
+		    }
+		);
+	    });
+    };
 
 
 
-var splitPaneSetup = function() {
+    var splitPaneSetup = function() {
 
-    var getDefn = function() {
-	return goog.dom.getElementsByTagNameAndClass(
-	    'div', 'CodeMirror-wrapping',
-	    document.getElementById("definitions"))[0];
-    }
-
-
-
-    var top = document.getElementById("top");
-    var bottom = document.getElementById("bottom");
-    var middle = document.getElementById('middle');
-
-
-    var splitpaneDiv = document.getElementById('splitpane');
-    var definitions = document.getElementById("definitions");
-    var defn = getDefn();
-
-    var interactions = document.getElementById("interactions");
-
-
-    var splitpane1 = new goog.ui.SplitPane(
-	new goog.ui.Component(),
-	new goog.ui.Component(),
-        goog.ui.SplitPane.Orientation.VERTICAL);
-    
-    splitpane1.decorate(splitpaneDiv);
-
-    var vsm = new goog.dom.ViewportSizeMonitor();
-
-
-
-    var currentSize = undefined;
-
-    // The display should consist of the top, the middle, and the bottom.
-    // The middle should expand to the size of the viewport minus the top and bottom.
-    var onResize = function(e) {
-
-	var viewportSize = vsm.getSize();
-	var desiredWidth = viewportSize.width;
-	var desiredHeight = (viewportSize.height - 
-			     goog.style.getBorderBoxSize(top).height - 
-			     goog.style.getBorderBoxSize(bottom).height);
-
-	var newSize = new goog.math.Size(desiredWidth, desiredHeight);
-	if ((! currentSize) ||
-	    (! goog.math.Size.equals(currentSize, newSize))) {
-            currentSize = newSize;
-            splitpane1.setSize(newSize);
-	    goog.style.setBorderBoxSize(middle, newSize);
+	var getDefn = function() {
+	    return goog.dom.getElementsByTagNameAndClass(
+		'div', 'CodeMirror-wrapping',
+		document.getElementById("definitions"))[0];
 	}
 
-	goog.style.setBorderBoxSize(splitpaneDiv,
-				    newSize);
-	synchronize();
-    };
+
+
+	var top = document.getElementById("top");
+	var bottom = document.getElementById("bottom");
+	var middle = document.getElementById('middle');
+
+
+	var splitpaneDiv = document.getElementById('splitpane');
+	var definitions = document.getElementById("definitions");
+	var defn = getDefn();
+
+	var interactions = document.getElementById("interactions");
+
+
+	var splitpane1 = new goog.ui.SplitPane(
+	    new goog.ui.Component(),
+	    new goog.ui.Component(),
+            goog.ui.SplitPane.Orientation.VERTICAL);
+	
+	splitpane1.decorate(splitpaneDiv);
+
+	var vsm = new goog.dom.ViewportSizeMonitor();
 
 
 
-    var synchronize = function() {
-	synchronizeTopSize();
-	synchronizeCodeMirror();
-    };
+	var currentSize = undefined;
 
+	// The display should consist of the top, the middle, and the bottom.
+	// The middle should expand to the size of the viewport minus the top and bottom.
+	var onResize = function(e) {
 
-    var synchronizeTopSize = function() {
-	goog.style.setBorderBoxSize(
-	    defn,
-	    goog.style.getBorderBoxSize(definitions));
-    };
+	    var viewportSize = vsm.getSize();
+	    var desiredWidth = viewportSize.width;
+	    var desiredHeight = (viewportSize.height - 
+				 goog.style.getBorderBoxSize(top).height - 
+				 goog.style.getBorderBoxSize(bottom).height);
 
-
-    var synchronizeCodeMirror = function() {
-	// HACK: get the width of the internal frame of the editor to match
-	// the viewport, taking into account the width of the line numbers.
-	var wrapping = goog.dom.getElementsByTagNameAndClass(
-	    'div', 'CodeMirror-wrapping', definitions);
-
-	for (var i = 0 ; i < wrapping.length; i++) {
-	    var wrappingSize = goog.style.getBorderBoxSize(wrapping[i]);
-	    var lineNumberWidth;
-	    var lineNumberDivs = 
-		goog.dom.getElementsByTagNameAndClass('div', 
-						      'CodeMirror-line-numbers',
-						      wrapping[i]);
-	    for (var j = 0; j < lineNumberDivs.length; j++) {
-		lineNumberWidth = goog.style.getBorderBoxSize(lineNumberDivs[j]).width;
+	    var newSize = new goog.math.Size(desiredWidth, desiredHeight);
+	    if ((! currentSize) ||
+		(! goog.math.Size.equals(currentSize, newSize))) {
+		currentSize = newSize;
+		splitpane1.setSize(newSize);
+		goog.style.setBorderBoxSize(middle, newSize);
 	    }
 
-	    if (typeof(lineNumberWidth) !== 'undefined') {
-		var iframes = 
-		    goog.dom.getElementsByTagNameAndClass(
-			'iframe', undefined, wrapping[i]);
-		for (var j = 0; j < iframes.length; j++) {
-		    var iframe = iframes[j];
-		    var iframeBox = goog.style.getBorderBoxSize(iframe);
-
-		    iframe.style.width = (vsm.getSize().width - lineNumberWidth) + "px";
-		}
-	    }
+	    goog.style.setBorderBoxSize(splitpaneDiv,
+					newSize);
+	    synchronize();
 	};
+
+
+
+	var synchronize = function() {
+	    synchronizeTopSize();
+	    synchronizeCodeMirror();
+	};
+
+
+	var synchronizeTopSize = function() {
+	    goog.style.setBorderBoxSize(
+		defn,
+		goog.style.getBorderBoxSize(definitions));
+	};
+
+
+	var synchronizeCodeMirror = function() {
+	    // HACK: get the width of the internal frame of the editor to match
+	    // the viewport, taking into account the width of the line numbers.
+	    var wrapping = goog.dom.getElementsByTagNameAndClass(
+		'div', 'CodeMirror-wrapping', definitions);
+
+	    for (var i = 0 ; i < wrapping.length; i++) {
+		var wrappingSize = goog.style.getBorderBoxSize(wrapping[i]);
+		var lineNumberWidth;
+		var lineNumberDivs = 
+		    goog.dom.getElementsByTagNameAndClass('div', 
+							  'CodeMirror-line-numbers',
+							  wrapping[i]);
+		for (var j = 0; j < lineNumberDivs.length; j++) {
+		    lineNumberWidth = goog.style.getBorderBoxSize(lineNumberDivs[j]).width;
+		}
+
+		if (typeof(lineNumberWidth) !== 'undefined') {
+		    var iframes = 
+			goog.dom.getElementsByTagNameAndClass(
+			    'iframe', undefined, wrapping[i]);
+		    for (var j = 0; j < iframes.length; j++) {
+			var iframe = iframes[j];
+			var iframeBox = goog.style.getBorderBoxSize(iframe);
+
+			iframe.style.width = (vsm.getSize().width - lineNumberWidth) + "px";
+		    }
+		}
+	    };
+	};
+
+
+	goog.events.listen(splitpane1,
+			   goog.events.EventType.CHANGE,
+			   synchronize);
+
+	goog.events.listen(vsm,
+			   goog.events.EventType.RESIZE,
+			   onResize);
+
+	setTimeout(onResize, 0);
     };
 
 
-    goog.events.listen(splitpane1,
-		       goog.events.EventType.CHANGE,
-		       synchronize);
-
-    goog.events.listen(vsm,
-                       goog.events.EventType.RESIZE,
-                       onResize);
-
-    setTimeout(onResize, 0);
-};
-
-
-
-
-function switchStyle(style){
-    document.getElementById('style').href = '/css/'+style;
-}
+    var switchStyle = function(style){
+	document.getElementById('style').href = '/css/'+style;
+    };
+})();
