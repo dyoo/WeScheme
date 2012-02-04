3cf8fe002836c9d12b99e6033fe57955ac731327
Danny Yoo <dyoo@cs.wpi.edu>
2012-02-04 17:52:18 -0500

    improving the script

--- bin/compress-js.rkt
+++ bin/compress-js.rkt
@@ -9,13 +9,18 @@
 ;; with simple optimizations.  All ".js" files (excluding the -min.js files)
 ;; get compressed here.
 
-(define path (command-line #:args (p) p))
+(define permissive? #f)
+(define quiet? #f)
+(define path (command-line #:once-each
+                           [("-q" "--quiet") "Quiet mode" (set! quiet? #t)]
+                           [("-p" "--permissive") "Permissive mode" (set! permissive? #t)]
+                           #:args (p) p))
 
 (define js-files (find-files
                   (lambda (p)
                     (and (file-exists? p)
                          (regexp-match #px".js$" (path->string (file-name-from-path p)))
-                         (not (regexp-match #px"-min.js$" (path->string (file-name-from-path p))))))
+                         (not (regexp-match #px"[.-]min.js$" (path->string (file-name-from-path p))))))
                   (simplify-path path)))
 
 ;; out-of-date?: path path -> boolean
@@ -28,12 +33,28 @@
     (>= (file-or-directory-modify-seconds source-file)
         (file-or-directory-modify-seconds target-file))]))
      
+
+(define (notify . args)
+  (unless quiet?
+    (apply printf args)))
+
 (for ([file js-files])
-   (define new-path (regexp-replace #px".js$" (path->string file) "-min.js"))
-   (cond [(out-of-date? file new-path)
-          (printf "Compressing ~s\n" (path->string file))
-          (define text (file->string file))
-          (define compressed (closure-compile text))
-          (call-with-output-file new-path (lambda (op) (display compressed op)) #:exists 'replace)]
-         [else
-          (printf "Skipping ~s: up to date\n" file)]))
+  (with-handlers ([exn:fail?
+                   (lambda (exn)
+                     (cond [permissive?
+                            (notify "Exception while handling ~a:\n---\n~a\n---\n"
+                                    (path->string file)
+                                    (exn-message exn))]
+                           [else
+                            (raise exn)]))])    
+    (define new-path (regexp-replace #px".js$" (path->string file) "-min.js"))
+    (cond [(out-of-date? file new-path)
+           (notify "Compressing ~s\n" (path->string file))
+           (define text (file->string file))
+           (define compressed (closure-compile text))
+           (call-with-output-file new-path (lambda (op)
+                                             (display compressed op))
+                                  #:exists 'replace)]
+          [else
+           (notify "Skipping ~s: up to date\n" file)])))
+
