34b5431f64b826ca0c2966e16dfb45c39986449b
Danny Yoo <dyoo@cs.wpi.edu>
2012-02-10 14:29:56 -0500

    fixing definition of isList so it runs in amortized constant time

--- war/js/mzscheme-vm/support.js
+++ war/js/mzscheme-vm/support.js
@@ -774,8 +774,40 @@ var helpers = {};
 	    return (x === types.EMPTY);
 	};
 
+
+
+
+    var isList = function(x) {
+        var tortoise, hare;
+        tortoise = hare = x;
+        if (hare === types.EMPTY) { 
+            return true; 
+        }
+        while (true) {
+            if (!(types.isPair(hare))) { return false; }
+            if (types.isPair(tortoise)) { 
+                // optimization to get amortized linear time isList.
+                if (tortoise._isList === true) { return true; } 
+                tortoise = tortoise.rest(); 
+            }
+            hare = hare.rest();
+            if (types.isPair(hare)) { 
+                if (hare._isList) { tortoise._isList = true; return true; }
+                hare = hare.rest(); 
+                if (types.isPair(hare) && hare._isList) { tortoise._isList = true; return true; }
+            }
+            if (hare === types.EMPTY) { 
+                // optimization to get amortized linear time isList.
+                tortoise._isList = true;
+                return true; 
+            }
+            if (tortoise === hare) { return false; }
+        }
+    };
+
+
 	var isListOf = function(x, f) {
-	    if (x === types.EMPTY) { return true; }
+            if (! isList(x)) { return false; }
 	    while (types.isPair(x)) {
 		if (! f(x.first())) { return false; }
 		x = x.rest();
