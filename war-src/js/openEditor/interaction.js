e8b1d6d29acbe07c124ebdd5a37a158c628ec82f
Mike Rowland <mjrowlan@cs.brown.edu>
2012-07-10 11:06:34 -0400

    changed coloring to match design docs- colors cycle and are more colorblind-friendly

--- war-src/js/openEditor/interaction.js
+++ war-src/js/openEditor/interaction.js
@@ -534,68 +534,66 @@ WeSchemeInteractions = (function () {
 
     // Special multi-color highlighting
     var specialFormatting = function(that, msgDom, msg) {
-	var colors = [new Color(238,169,184), new Color(100, 149, 240), new Color(124,205,124), 
-		      new Color(218,165,32), new Color(186,186,186)];
-	var colorIndex = 0; //WARNING
-	
-	var currItem;
-	var currColor = colors[colorIndex];
-	var args = msg.args;
-	for(var i = 0; i < args.length; i++){
-	    //in the unlikely event that there are no more preset colors, choose a random one
-	    if(colorIndex >= colors.length){
-		currColor = new Color(Math.floor(Math.random()*255), 
-				      Math.floor(Math.random()*255), 
-				      Math.floor(Math.random()*255));
-	    }
-	    else currColor = colors[colorIndex];
-	    
-	    if(types.isColoredPart(args[i])) {
-		currItem = args[i].location;
-		that.addToCurrentHighlighter(currItem.ref(0), currItem.ref(1), currItem.ref(2), currItem.ref(3), currItem.ref(4), currColor+'');
-		
-		var aChunk = jQuery("<span/>").text(args[i].text).css("background-color", currColor+'');
-		jQuery(msgDom).append(aChunk);
-		
-		colorIndex++;
-	    } else if(types.isGradientPart(args[i])) {
-		var parts = args[i].coloredParts;
-		
-		var percentage = 1;
-		var change = 1/(parts.length+1);
-		
-		var currTint;
-		for(var j = 0; j < parts.length; j++) {
-		    
-		    currItem = parts[j];
-		    currTint = nextTint(currColor.red, currColor.green, currColor.blue, percentage);
-		    
-		    that.addToCurrentHighlighter(currItem.location.ref(0), currItem.location.ref(1), currItem.location.ref(2), currItem.location.ref(3), currItem.location.ref(4), 
-						 currTint);
-		    
-		    var aChunk = jQuery("<span/>").text(currItem.text).css("background-color", currTint);
-		    jQuery(msgDom).append(aChunk);		     
-		    percentage = percentage - change;
-		}
-		
-		colorIndex++;
-	    }
-	    
-	    else if(types.isMultiPart(args[i])) {
-		var locs = args[i].locations;
-		
-		for(var j = 0; j <locs.length; j++){
-		    that.addToCurrentHighlighter(locs[j].ref(0), locs[j].ref(1), locs[j].ref(2), locs[j].ref(3), locs[j].ref(4), 
-						 currColor);
-		}
-		var aChunk = jQuery("<span/>").text(args[i].text).css("background-color", currColor+'');
-		jQuery(msgDom).append(aChunk);
-		
-		colorIndex++;
-	    } else {
-		msgDom.appendChild(document.createTextNode(args[i]+''));
-	    }
-	}	
+    	var colors = [new Color(238,169,184), new Color(145, 191, 219), new Color(127,191,123), 
+    		      new Color(175,141,195), new Color(186,186,186)];
+    	var colorIndex = 0;
+    	
+    	var currItem;
+    	var currColor = colors[colorIndex];
+    	var args = msg.args;
+    	for(var i = 0; i < args.length; i++){
+    	    //in the unlikely event that there are no more preset colors, reset it
+    	    if(colorIndex >= colors.length){
+        		colorIndex = 0;
+    	    }
+    	    currColor = colors[colorIndex];
+    	    
+    	    if(types.isColoredPart(args[i])) {
+    		currItem = args[i].location;
+    		that.addToCurrentHighlighter(currItem.ref(0), currItem.ref(1), currItem.ref(2), currItem.ref(3), currItem.ref(4), currColor+'');
+    		
+    		var aChunk = jQuery("<span/>").text(args[i].text).css("background-color", currColor+'');
+    		jQuery(msgDom).append(aChunk);
+    		
+    		colorIndex++;
+    	    } else if(types.isGradientPart(args[i])) {
+    		var parts = args[i].coloredParts;
+    		
+    		var percentage = 1;
+    		var change = 1/(parts.length+1);
+    		
+    		var currTint;
+    		for(var j = 0; j < parts.length; j++) {
+    		    
+    		    currItem = parts[j];
+    		    currTint = nextTint(currColor.red, currColor.green, currColor.blue, percentage);
+    		    
+    		    that.addToCurrentHighlighter(currItem.location.ref(0), currItem.location.ref(1), currItem.location.ref(2), currItem.location.ref(3), currItem.location.ref(4), 
+    						 currTint);
+    		    
+    		    var aChunk = jQuery("<span/>").text(currItem.text).css("background-color", currTint);
+    		    jQuery(msgDom).append(aChunk);		     
+    		    percentage = percentage - change;
+    		}
+    		
+    		colorIndex++;
+    	    }
+    	    
+    	    else if(types.isMultiPart(args[i])) {
+    		var locs = args[i].locations;
+    		
+    		for(var j = 0; j <locs.length; j++){
+    		    that.addToCurrentHighlighter(locs[j].ref(0), locs[j].ref(1), locs[j].ref(2), locs[j].ref(3), locs[j].ref(4), 
+    						 currColor);
+    		}
+    		var aChunk = jQuery("<span/>").text(args[i].text).css("background-color", currColor+'');
+    		jQuery(msgDom).append(aChunk);
+    		
+    		colorIndex++;
+    	    } else {
+    		msgDom.appendChild(document.createTextNode(args[i]+''));
+    	    }
+    	}	
     };
 
 
