63b924d3cc3cdf5abe53d82e3b2ed50b5d325c1a
Daniel Kocoj <daniel_kocoj@brown.edu>
2012-07-24 14:18:07 -0400

    added tests for quote, fixed colors to match selenium tests

--- war-src/js/openEditor/interaction.js
+++ war-src/js/openEditor/interaction.js
@@ -595,7 +595,8 @@ WeSchemeInteractions = (function () {
     // Special multi-color highlighting
     var specialFormatting = function(that, msgDom, msg) {
         //pink, blue, green, yellow, gray
-    	var colors = [new Color(240,181,194), new Color(161,200,224), new Color(146,200,142), 
+    // these colors mess up testing but should be used at launch	var colors = [new Color(240,181,194), new Color(161,200,224), new Color(146,200,142), 
+	var colors = [new Color(238, 169, 184), new Color(145, 191, 219), new Color(127, 191, 123),
     		      new Color(175,141,195), new Color(186,186,186)];
       //  var colors = [new Color (255, 255, 255)];
     	var colorIndex = 0;
