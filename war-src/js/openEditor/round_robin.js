37c25dc50434d824c8621f732ed09c6f81c3bbe2
Danny Yoo <dyoo@cs.wpi.edu>
2012-08-07 15:10:26 -0400

    Adjusted the compilation so it preferentially chooses the west or east coast servers based on latency to compile the empty program.

--- war-src/js/openEditor/round_robin.js
+++ war-src/js/openEditor/round_robin.js
@@ -2,18 +2,12 @@
 /*jslint browser: true, vars: true, white: true, plusplus: true, maxerr: 50, indent: 4 */
 
 goog.provide("plt.wescheme.RoundRobin");
-goog.require("plt.wescheme.WeSchemeProperties");
 
 
 (function() {
     "use strict";
     //////////////////////////////////////////////////////////////////////
 
-    // Initializes the evaluator to use round-robin compilation, given a list of
-    // servers.
-    var getServerList = function() {
-        return plt.wescheme.WeSchemeProperties.compilation_servers.split(/\s+/);
-    };
 
 
     // Initializes the remote procedure call between the client and
@@ -21,7 +15,7 @@ goog.require("plt.wescheme.WeSchemeProperties");
     // need to ping the server up front to see if the connection is
     // alive: we don't get a reliable error exception if the server is
     // down from the very beginning.
-    var initializeXhr = function(serverUrl, afterInitialize) {
+    var initializeServer = function(serverUrl, afterInitialize) {
         var xhr = new easyXDM.Rpc(
             { remote: serverUrl,
               // This lazy flag must be set to avoid a very ugly
@@ -32,24 +26,38 @@ goog.require("plt.wescheme.WeSchemeProperties");
         // We initiate compilation of the empty program and see
         // if the server responds.  If it does, we add the server
         // to the list of known good servers.
+        var startTime = new Date();
         xhr.compileProgram("", 
                            "",
                            function(bytecode) {
-                               xhrs.push(xhr);
+                               liveServers.push( { xhr : xhr,
+                                                   url : serverUrl,
+                                                   pingDelay : (new Date() - startTime) } );
                                if (! AT_LEAST_ONE_SERVER_READY) {
                                    AT_LEAST_ONE_SERVER_READY = true;
                                    afterInitialize();
                                }
+                               // Sort the servers in ascending pingDelay.
+                               liveServers.sort(
+                                   function(x, y) {
+                                       if (x.pingDelay < y.pingDelay) { 
+                                           return -1;
+                                       }
+                                       if (x.pingDelay > y.pingDelay) {
+                                           return 1;
+                                       }
+                                       return 0;
+                                   });
                            },
                            function(err) {
                            });
     };
 
 
-    var xhrs = [];
+    var liveServers = [];
 
     var AT_LEAST_ONE_SERVER_READY = false;
-    var initialize = function(afterInitialize, onFailure) {
+    var initialize = function(compilation_servers, afterInitialize, onFailure) {
         if (AT_LEAST_ONE_SERVER_READY) {
             afterInitialize();
         } else {
@@ -63,13 +71,22 @@ goog.require("plt.wescheme.WeSchemeProperties");
             // Configures the evaluator to use round-robin compilation between
             // a set of servers.  Compilation will also fall back to other
             // servers under network failure.
-            var compilation_servers = getServerList();
             var i;
             for (i = 0; i < compilation_servers.length; i++) {
-                initializeXhr(compilation_servers[i], afterInitialize);
+                initializeServer(compilation_servers[i], afterInitialize);
             }
         }
     };
+
+
+    // Resets the round robin servers to the initial state.  Use
+    // initialize() to reconfigure.
+    var reset = function() {
+        AT_LEAST_ONE_SERVER_READY = false;
+        liveServers = [];
+    };
+
+
 
 
 
@@ -85,13 +102,13 @@ goog.require("plt.wescheme.WeSchemeProperties");
 
     // Try using server n to compile the expression.  If network
     // failure occurs, try the next one in round-robin order, up
-    // to xhrs.length tries.
+    // to liveServers.length tries.
     var tryServerN = function(n, countFailures, 
                               programName, code, 
                               onDone, onDoneError) {
 
-        if (n < xhrs.length) {
-            xhrs[n].compileProgram(
+        if (n < liveServers.length) {
+            liveServers[n].xhr.compileProgram(
                 programName,
                 code,
                 onDone,
@@ -100,10 +117,10 @@ goog.require("plt.wescheme.WeSchemeProperties");
                     // empty string, communication with
                     // the server failed.
                     if (errorStruct.message === "") {
-                        if (countFailures >= xhrs.length) {
+                        if (countFailures >= liveServers.length) {
                             onAllCompilationServersFailing(onDoneError);
                         } else {
-                            tryServerN(((n + 1) % xhrs.length),
+                            tryServerN(((n + 1) % liveServers.length),
                                        countFailures + 1,
                                        programName,
                                        code,
@@ -120,12 +137,12 @@ goog.require("plt.wescheme.WeSchemeProperties");
     };
 
 
-    var roundRobinIndex = 0;
+    // The name "round-robin" is historical: we really contact the
+    // servers in order, starting from liveServers[0], liveServers[1], ...
     var roundRobinCompiler = 
         function(programName, code, onDone, onDoneError) {
-            if (xhrs.length > 0) {
-                roundRobinIndex = (((roundRobinIndex + 1) % xhrs.length));
-                tryServerN(roundRobinIndex, 0,
+            if (liveServers.length > 0) {
+                tryServerN(0, 0,
                            programName, code, onDone, onDoneError);
             } else {
                 onAllCompilationServersFailing(onDoneError);
@@ -135,8 +152,12 @@ goog.require("plt.wescheme.WeSchemeProperties");
     
 
     //////////////////////////////////////////////////////////////////////
+
     plt.wescheme.RoundRobin.initialize = initialize;
     plt.wescheme.RoundRobin.roundRobinCompiler = roundRobinCompiler;
 
+    
+    // The following exports are for debugging purposes.
+    plt.wescheme.RoundRobin.reset = reset;
+    plt.wescheme.RoundRobin.liveServers = liveServers;
 }());
-
