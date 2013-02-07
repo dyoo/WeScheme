0f4f8ce7b4fd1c7a9f429775265cf8d6d2a7548c
Danny Yoo <dyoo@hashcollision.org>
2013-02-07 14:57:44 -0700

    trying to get test cases in place.

---
+++ test-src/org/wescheme/MyFirstTest.java
@@ -0,0 +1,10 @@
+package org.wescheme;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MyFirstTest {
+    @Test public void testAddition() {
+        assertEquals(5, 2+2);
+    }
+}
