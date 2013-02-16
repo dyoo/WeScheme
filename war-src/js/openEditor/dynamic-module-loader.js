5ac4f7e4c2572e6458d526dca7d505360ad6fedc
Danny Yoo <dyoo@hashcollision.org>
2013-02-15 17:09:25 -0700

    module require is doing the right thing.

--- war-src/js/openEditor/dynamic-module-loader.js
+++ war-src/js/openEditor/dynamic-module-loader.js
@@ -1,10 +1,16 @@
 goog.provide("plt.wescheme.makeDynamicModuleLoader");
 
+goog.require("plt.wescheme.AjaxActions");
 
 (function() {
+    'use strict';
+
+
     // The dynamic module loader will either cause bytecode from publicId-shared
     // programs to load, or those in the built-in collections.
     plt.wescheme.makeDynamicModuleLoader = function(rootLibraryPath) {
+
+        var actions = new plt.wescheme.AjaxActions();
 
         var handleBuiltInCollection = function(aName, onSuccess, onFail) {
             loadScript(rootLibraryPath + "/" + aName + "-min.js?__gensym="+encodeURIComponent('' + Math.random()),
@@ -13,12 +19,29 @@ goog.provide("plt.wescheme.makeDynamicModuleLoader");
         };
 
         var handleWeschemeModule = function(aName, onSuccess, onFail) {
-            var m = aName.match(/wescheme\/(\\w+)/);
-            throw new Error("unimplemented.");
+            var m = aName.match(/wescheme\/(\w+)/);
+            var publicId = m[1];
+            actions.loadProject(
+                null, 
+                publicId, 
+                function(aProgram) {
+                    try {
+                        window.COLLECTIONS[aName] = { 
+                            'name': aName,  
+                            'bytecode' : eval('(' + aProgram.getObjectCode() + ')'),
+                            'provides' : aProgram.getProvides() 
+                        };
+                    } catch(e) {
+                        onFail();
+                        return;
+                    }
+                    onSuccess();
+                },
+                onFail);
         };
 
         var isWeschemeModule = function(aName) {
-            var m = aName.match(/wescheme\/(\\w+)/);
+            var m = aName.match(/^wescheme\/(\w+)$/);
             if (m) { return true; }
             return false;
         };
