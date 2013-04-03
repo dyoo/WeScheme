a6668e031be60de8a8b4418304de85a1072d5e9f
Danny Yoo <dyoo@hashcollision.org>
2013-04-02 18:06:44 -0600

    refactoring prompt into separate file.

--- war-src/js/openEditor/interaction.js
+++ war-src/js/openEditor/interaction.js
@@ -15,13 +15,13 @@ goog.require("plt.wescheme.tokenizer");
 goog.require("plt.wescheme.WeSchemeProperties");
 goog.require("plt.wescheme.RoundRobin");
 goog.require("plt.wescheme.makeDynamicModuleLoader");
+goog.require("plt.wescheme.prompt");
 
 var WeSchemeInteractions;
 
 WeSchemeInteractions = (function () {
     'use strict';
 
-    var Prompt, makeFreshId;
 
     // WeSchemeInteractions: div (WeSchemeInteractions -> void) -> WeScheme
     var WeSchemeInteractions = function(interactionsDiv, afterInit) {
@@ -38,7 +38,7 @@ WeSchemeInteractions = (function () {
         this.withColoredErrorMessages = true;
 
 
-        new Prompt(
+        plt.wescheme.prompt.makePrompt(
             this,
             this.interactionsDiv,
             function(prompt) {
@@ -120,212 +120,6 @@ WeSchemeInteractions = (function () {
     // Sets the text in the prompt.
     WeSchemeInteractions.prototype.setPromptText = function(t) {
         this.prompt.setText(t);
-    };
-
-    //////////////////////////////////////////////////////////////////////
-    Prompt = function(interactions, parentDiv, K) {
-        var that = this;
-        this.interactions = interactions;
-        this.div = jQuery("<div><span class='top-aligned-inline-block'>&gt;&nbsp;</span><span class='top-aligned-inline-block' style='width: 90%'/></div>");
-        parentDiv.append(this.div);
-
-        var innerDivElt = this.div.find("span").get(1);
-        new plt.wescheme.WeSchemeTextContainer(
-            innerDivElt,
-            { dynamicHeight: true,
-              lineNumbers: false,
-              theme: "scheme-interactive",
-              //stylesheet: "/js/codemirror/contrib/scheme/css/schemecolors-interactive.css",
-              makeTransparentIframe: true,
-              extraKeys: {
-                  "Enter":function (ed) {
-                      if (that.hasCompleteExpression()) {
-                          that.onEvaluation();
-                      } else {
-                          CodeMirror.commands.newlineAndIndent(ed);
-                      }
-                  },
-                  "Ctrl-N":function (ed) {
-                      that.onHistoryNext();
-                  },
-                  "Ctrl-P":function (ed) {
-                      that.onHistoryPrevious();
-                  },
-                  "Alt-N":function (ed) {
-                      that.onHistoryNext();
-                  },
-                  "Alt-P":function (ed) {
-                      that.onHistoryPrevious();
-                  }
-              }},
-            function(container) {
-                that.textContainer = container;
-
-                if (K) {
-                    K(that);
-                }
-            });
-    };
-
-    Prompt.prototype.onEvaluation = function() {
-        this.saveHistoryWithCleanup();
-        var that = this;
-        var nextCode = that.textContainer.getCode();
-        that.textContainer.setCode("");
-
-        var parentDiv = document.createElement('div');
-
-        var promptSpan = document.createElement('span');
-        promptSpan.className = 'top-aligned-inline-block';
-        promptSpan.appendChild(document.createTextNode(">"));
-
-        var textareaSpan = document.createElement("span");
-        textareaSpan.className = 'top-aligned-inline-block';
-        textareaSpan.style.width = '90%';
-
-        parentDiv.appendChild(promptSpan);
-        parentDiv.appendChild(document.createTextNode(" "));
-        parentDiv.appendChild(textareaSpan);
-        that.interactions.addToInteractions(parentDiv);
-
-        that.interactions.clearLine();
-        // // FIXME: figure out how to get the line height
-        // dynamically, because I have no idea how to do
-        // this correctly at the moment.
-        var n = new plt.wescheme.WeSchemeTextContainer(
-            textareaSpan,
-            {   dynamicHeight: true,
-                lineNumbers: false,
-                theme: "scheme-interactive",
-                //stylesheet: "/js/codemirror/contrib/scheme/css/schemecolors-interactive.css",
-                content: nextCode,
-                makeTransparentIframe: true,
-                readOnly: true,
-                matchBrackets: false },
-            function(container) {
-                var newId = makeFreshId();
-                that.interactions.previousInteractionsTextContainers[newId] = container;
-                that.interactions.runCode(newId, nextCode, function() {});
-            });
-        //calling that.focus() doesn't work - the codeMirror box looks focused, but you can't type into it
-        //if I focus on something else first, everything works fine
-        n.focus();
-        that.focus();
-    };
-
-    // TODO: historyPreviousIsOk and historyNextIsOk don't have to be methods.
-
-    Prompt.prototype.historyPreviousIsOk = function(index, length) {
-        return (index > 0);
-    };
-
-    Prompt.prototype.historyNextIsOk = function(index, length) {
-        return ((length - index) > 1);
-    }
-
-    Prompt.prototype.onHistoryPrevious = function() {
-        this.doHistory(-1, this.historyPreviousIsOk);
-    };
-
-    Prompt.prototype.onHistoryNext = function() {
-        this.doHistory(1, this.historyNextIsOk);
-    };
-
-    Prompt.prototype.doHistory = function(increment, incrementIsOk) {
-        if (this.historyArray.length > 1) {
-            var code = jQuery.trim(this.textContainer.getCode());
-            if (code === this.historyArray[this.historyIndex]) {
-                // The code *is* the same as the history slot, so do the increment if OK.
-                if (incrementIsOk(this.historyIndex, this.historyArray.length)) {
-                    this.doHistoryPart2(increment);
-                }
-            } else {
-                // The code is *not* the same as the history slot, so,
-                // if increment will be OK if we update the last slot
-                // with the current code, then update the last slot and
-                // then do the increment.
-                var tentativeIndex = (this.historyArray.length - 1);
-                if (incrementIsOk(tentativeIndex, this.historyArray.length)) {
-                    this.historyIndex = tentativeIndex;
-                    this.historyArray[this.historyIndex] = code;
-                    this.doHistoryPart2(increment);
-                }
-            }
-        }
-
-    };
-
-    Prompt.prototype.doHistoryPart2 = function(increment) {
-        this.historyIndex += increment;
-        this.textContainer.setCode(this.historyArray[this.historyIndex]);
-        // TODO: !!! setCursorToEnd doesn't yet work.
-        this.textContainer.setCursorToEnd();
-    };
-
-    Prompt.prototype.saveHistoryWithCleanup = function() {
-        var newEntry = jQuery.trim(this.textContainer.getCode());
-        var lastIndex = (this.historyArray.length - 1);
-        this.historyArray[lastIndex] = newEntry;
-        var prevEntry = ((lastIndex < 1) ? null : this.historyArray[lastIndex - 1]);
-        if (prevEntry && (prevEntry === newEntry)) {
-            // The new entry is the same as the previous, so fall through and
-            // let the new entry be reset to a blank.
-        } else if (newEntry === "") {
-            // New entry is already blank, so don't need to add a new one.
-        } else {
-            // Need to add a blank to historyArray, so shrink historyArray
-            // if necessary, and then add blank.
-            var shiftCount = Math.max(0, (this.historyArray.length - this.maxSavedHistory));
-            while (shiftCount > 0) {
-                this.historyArray.shift();
-                shiftCount--;
-            }
-            // Note: We are setting lastIndex to one greater than the
-            // current last index, so that our use of it in as an array
-            // index in lhs of assignment below will result in appending to
-            // the array.
-            lastIndex = this.historyArray.length;
-        }
-        this.historyArray[lastIndex] = "";
-        this.historyIndex = lastIndex;
-    };
-
-    // hasExpressionToEvaluate: -> boolean
-    // Return true if the prompt contains a complete expression
-    Prompt.prototype.hasCompleteExpression = function() {
-        var codePastCursor = this.textContainer.getCode(this.textContainer.getCursorStartPosition());
-        if (codePastCursor.match(new RegExp("[^\\s]"))) {
-            return false;
-        }
-        var codeUpToCursor = this.textContainer.getCode(0, this.textContainer.getCursorStartPosition());
-        return plt.wescheme.tokenizer.hasCompleteExpression(codeUpToCursor);
-    };
-
-
-
-    Prompt.prototype.setText = function(t) {
-        this.textContainer.setCode(t);
-    };
-
-    Prompt.prototype.clear = function() {
-        this.textContainer.setCode("");
-    };
-
-    Prompt.prototype.getDiv = function() {
-        return this.div;
-    };
-
-    Prompt.prototype.hide = function() {
-        this.div.hide();
-    };
-
-    Prompt.prototype.show = function() {
-        this.div.show();
-    };
-
-    Prompt.prototype.focus = function() {
-        this.interactions._scrollToBottom();
-        this.textContainer.focus();
     };
 
 
@@ -1111,12 +905,6 @@ WeSchemeInteractions = (function () {
 
     WeSchemeInteractions.prototype.toString = function() { return "WeSchemeInteractions()"; };
 
-    //////////////////////////////////////////////////////////////////////
-    var _idNum = 0;
-    makeFreshId = function() {
-        return ("<interactions" + (_idNum++) + ">");
-    }
-    //////////////////////////////////////////////////////////////////////
 
     return WeSchemeInteractions;
 })();
